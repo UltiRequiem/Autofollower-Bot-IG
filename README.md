6a757328a165a0253242bbb9f3c13ea2d0e4ce9d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-04-04 17:41:10 -0700

    ✨ Update

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ A simple autofollow bot for [Instagram](https://www.instagram.com/).
 * PIP3
 
 ### Installing
-* Make a git clone or dowloand the zip of the proyect
+* Git clone the proyect
 * Install Selenium module:
 ```bash
 pip install Selenium
@@ -18,13 +18,8 @@ pip install Selenium
 * Get in the Autofollow-Bot-for-Instagram directory
 * Open a terminal in the directory
 ```
-python main.py
+python3 main.py
 ```
-
-### Tutorial:
-[![Watch the video](https://i.ytimg.com/an_webp/el1b7AgsShw/mqdefault_6s.webp?du=3000&sqp=CLzrloAG&rs=AOn4CLD0tdIiVc7j0J3P2jcSz6Au-fswaA)](https://youtu.be/el1b7AgsShw)
-
-**Click it**
 
 ## Help
 [![Twitter Badge](https://img.shields.io/badge/-@EliazBobadilla-00acee?style=flat&logo=twitter&logoColor=white&link=https://twitter.com/@EliazBobadilla/)](https://www.twitter.com/@EliazBobadilla/)
--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-from selenium import webdriver
 from time import sleep
-driver = webdriver.Chrome()
-driver.get("https://instagram.com")
-sleep(4)
-driver.find_element_by_xpath("//input[@name=\"username\"]")\
-    .send_keys("Ur Username")
-driver.find_element_by_xpath("//input[@name=\"password\"]")\
-    .send_keys("Ur Password")
-driver.find_element_by_xpath('/button[@type="submit"]')\
-    .click()
-sleep(4)
+from selenium import webdriver
 
-driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
-    .click()
-sleep(3)
-
-for i in range(3):
-    for i in range(5):
-        driver.find_element_by_xpath('//button[text()="Follow"]')\
+def run():
+    driver = webdriver.Chrome(executable_path="./path")
+    driver.get("https://instagram.com")
+    sleep(4)
+    
+    driver.find_element_by_xpath("//input[@name=\"username\"]")\
+            .send_keys("Your Username")
+    driver.find_element_by_xpath("//input[@name=\"password\"]")\
+            .send_keys("Your Password")
+    driver.find_element_by_xpath('/button[@type="submit"]')\
             .click()
+    sleep(4)
+
+    driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
+    sleep(3)
+
+    for item in range(3):
+        for item in range(5):
+            driver.find_element_by_xpath('//button[text()="Follow"]')\
+                    .click()
         sleep(1)
     driver.refresh()
+
+if __name__ == '__main__':
+    run()
