1fc9c7eb6fe7bc77aac2e12ad254d429ee827e4c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-14 14:32:57 -0500

    :lock: Archive

--- README.md
+++ README.md
@@ -1,21 +1,33 @@
+**Instagram has changed its frontend, so this script no longer works, feel free to adapt it.**
+
 # Autofollow Bot for IG
+
+<p>
+<a href="https://github.com/UltiRequiem/Autofollower-Bot-IG/blob/main/LICENSE"><img alt="License: MIT" src="https://black.readthedocs.io/en/stable/_static/license.svg"></a>
+<a href="https://github.com/UltiRequiem/Autofollower-Bot-IG"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a>
+<a href="https://github.com/UltiRequiem/Autofollower-Bot-IG"><img alt="Code style: black" src="https://img.shields.io/tokei/lines/github.com/UltiRequiem/Autofollower-Bot-IG?color=blue&label=Total%20Lines"></a>
+</p>
 
 A simple bot for [Instagram](https://www.instagram.com).
 
 ### Dependencies
 
-* A 3.X Python version
-* PIP3
-* Selenium
+- A 3.X Python version
+- PIP3
+- Selenium
 
 ### Installation
-* Git clone the proyect
-* Install Selenium module:
+
+- Git clone the proyect
+- Install Selenium module:
+
 ```bash
 pip install Selenium
 ```
-* Get in the Autofollow-Bot-for-Instagram directory
-* Open a terminal in the directory
+
+- Get in the Autofollow-Bot-for-Instagram directory
+- Open a terminal in the directory
+
 ```
-python3 main.py
+python main.py
 ```
--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from time import sleep
 from selenium import webdriver
 
+
 def run():
     driver = webdriver.Chrome(executable_path="./path")
     driver.get("https://instagram.com")
     sleep(4)
-    driver.find_element_by_xpath("//input[@name=\"username\"]")\
-            .send_keys("Your Username")
-    driver.find_element_by_xpath("//input[@name=\"password\"]")\
-            .send_keys("Your Password")
-    driver.find_element_by_xpath('/button[@type="submit"]')\
-            .click()
+    driver.find_element_by_xpath('//input[@name="username"]').send_keys("Your Username")
+    driver.find_element_by_xpath('//input[@name="password"]').send_keys("Your Password")
+    driver.find_element_by_xpath('/button[@type="submit"]').click()
     sleep(4)
 
-    driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
-            .click()
+    driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
     sleep(3)
 
     for item in range(3):
         for i in range(5):
-            driver.find_element_by_xpath('//button[text()="Follow"]')\
-                    .click()
+            driver.find_element_by_xpath('//button[text()="Follow"]').click()
         sleep(1)
     driver.refresh()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
